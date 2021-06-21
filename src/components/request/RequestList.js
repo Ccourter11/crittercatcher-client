37a5ab3fa791c42af157dc9694e6a842da50b30d
Ccourter11 <courtercalvin@yahoo.com>
2021-06-21 14:00:33 -0500

    fix: can now see date and time correctly, also applied some styling

--- src/components/request/RequestList.js
+++ src/components/request/RequestList.js
@@ -3,6 +3,7 @@ import { RequestContext } from "./RequestProvider.js"
 import { useHistory } from "react-router-dom"
 import Button from 'react-bootstrap/Button'
 import "./Request.css"
+import moment from "moment"
 
 export const RequestList = () => {
     const { requests, getRequests, deleteRequest } = useContext(RequestContext)
@@ -33,10 +34,12 @@ export const RequestList = () => {
                 requests.map(request => {
                     return <section key={`request--${request.id}`} className="request">
                         
-                        <div className="request__title">Title: {request.title}</div>
-                        <div className="request__description">Description: {request.description} </div>
-                        <div className="request__location">Location: {request.location}</div>
-                        <div className="request__datetime">Date: {request.date}</div>
+                        <div className="request__title"><strong>Title:</strong> {request.title}</div>
+                        <div className="request__description"><strong>Description:</strong> {request.description} </div>
+                        <div className="request__location"><strong>Location:</strong> {request.location}</div>
+                        <div className="request__time"><strong>Time:</strong> {moment(request.datetime).format('LT')}</div>   
+                        <div className="request__time"><strong>Date:</strong> {moment(request.datetime).format('L')}</div>   
+                        
 
                         <div className="request__edit">
                              <Button variant="warning" className="btn btn-3"
