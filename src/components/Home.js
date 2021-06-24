58341d685671c15d0224a451fcdbf5012cee111c
Ccourter11 <courtercalvin@yahoo.com>
2021-06-24 11:48:21 -0500

    feat: login wallpaper

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ export const Home = () => {
     return  <>
                 <div>
                     <p>Critter Catcher of Middle Tennessee</p>
+                    <p>Are you in need of a reliable and professional animal removal company in Nashville, TN? Look no further than Critter Catchers. Our trained and nationally certified technicians will perform a thorough inspection of your Nashville home or business. Not only do we specialize in animal removal, but we also perform animal prevention and sanitation. Schedule a request!</p>
                 </div>
             </>
 }
